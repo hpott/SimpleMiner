8d983ff5cdd0c5f0c45df1bf0ac74600693c1bd6
Hugo Potter <hugpot91@icloud.com>
2020-06-07 17:00:59 +1000

    Added Support for Ethereum Mining
    Added support for ETH mining with Claymore's Dual Ethereum Miner using pool ethermine.org.

---
+++ ETH.bat
@@ -0,0 +1,1 @@
+bin\claymore\EthDcrMiner64.exe -pool us1.ethermine.org:4444 -wal %1 -worker SimpleMiner 
--- sm.py
+++ sm.py
@@ -1,35 +1,52 @@
-from Tkinter import *
+from tkinter import *
 import os
 import subprocess
 
 
-cmd='config.bat '
-
-
+cmd='.bat '
 
 master = Tk()
+master.geometry("250x150")
+master.title('SimpleMiner')
 
 e = Entry(master)
 e.pack()
 
 e.focus_set()
+def taskkill():
+	if coin.get()=="ETH":
+		subprocess.call("taskkill /f /im EthDcrMiner64.exe", shell=False)
+	elif coin.get()=="XMR":
+		subprocess.call("taskkill /f /im xmrig.exe", shell=False)
+	
+		
+def taskpause():
+	taskkill()
 def taskend():
-   subprocess.call("taskkill /f /im xmrig.exe", shell=True)
+   taskkill()
    quit()
 
 def callback():
-   xmr=e.get()
-   print xmr
-   subprocess.Popen(cmd+xmr)
+   	wal=e.get()
+   	subprocess.Popen(coin.get()+cmd+wal)
+coin = StringVar(master)
+coin.set("XMR")
+
+menu = OptionMenu(master, coin, "XMR", "ETH")
+menu.pack()
 start = Button(master, text="Start mining", width=10, command=callback)
 start.pack()
+pause=Button(master, text="Pause Mining", width=10, command=taskpause)
+pause.pack()
 stop = Button(master, text="Stop Mining", width=10, command=taskend)
 stop.pack()
 
 
 
+
 mainloop()
 e = Entry(master, width=50)
+e.title
 e.pack()
 
 text = e.get()
